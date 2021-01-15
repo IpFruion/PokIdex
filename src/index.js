6619339a8498f3fdc26f5b9452cb132785bbd47b
Derrick Lockwood <lockwood.derrick@gmail.com>
2021-01-15 12:17:50 -0600

    Updated with Bootstrap and added some error handling

--- src/index.js
+++ src/index.js
@@ -2,81 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import { useState } from 'react';
-// import { Container, Row, Alert, Button } from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import { Scanner } from './scanner.js';
 import { Profile } from './profile.js';
-import { Popup, DexProfile } from './util.js';
 import { NewCard } from './newcard.js';
-import './index.css';
-
-function LoadFile(props) {
-  const [verify, setVerify] = React.useState(null);
-  const [file, setFile] = React.useState(null);
-  function loadedFile(event) {
-    setFile(event.target.files[0]);
-    setVerify((<button onClick={() => props.onFinished(file)}>Accept</button>));
-  }
-  const lf = (
-    <div className="loadfile">
-      <h1>Input Dex File</h1>
-      <input type="file" name=".dex file" accept=".dex" onChange={loadedFile}/>
-      {verify}
-    </div>
-  );
-  return <Popup blur={props.blur} children={lf} onFinished={() => props.onFinished(null)}/>
-}
-
-class LoadDex extends React.Component {
-  constructor(props) {
-    super(props);
-    this.main = React.createRef();
-    this.state = {
-      loadFileMenu: null,
-    }
-  }
-
-  async uploadFile(file) {
-    const reader = new FileReader()
-    reader.onLoad = function (event) {
-      this.props.onLoadedDex(JSON.parse(event.target.result));
-    }
-    reader.readAsText(file);
-  }
-
-  showLoadFile(isLoad) {
-    let loadFileMenu = null;
-    if (isLoad) {
-      loadFileMenu = <LoadFile blur={this.main} onFinished={(file) => {
-        if (file) {
-          this.uploadFile(file);
-        }
-        this.showLoadFile(false);
-      }}/>;
-    }
-    this.setState({...this.state, loadFileMenu: loadFileMenu});
-  }
-
-  render() {
-    return (
-      <Route path="/" render={({history}) => (
-        <div className="container">
-          {this.state.loadFileMenu}
-          <div ref={this.main} className="main">
-            <h1>PokIndex</h1>
-            <button onClick={() => this.showLoadFile(true)}>Load .dex file</button>
-            <h2>or</h2>
-            <button onClick={() => {
-              this.props.onLoadedDex(new DexProfile());
-              history.push("/profile");
-            }}>Create new PokIndex</button>
-          </div>
-        </div>
-      )}/>
-
-    );
-  }
-}
+import { LoadDex } from './load.js';
+import './index.scss';
 
 // function NotFound(props) {
 //   let loc = "/";
@@ -99,33 +31,52 @@ class App extends React.Component {
     super(props);
     this.state = {
       dexProfile: null,
+      error: null
     }
     this.addCardToDex = this.addCardToDex.bind(this);
   }
 
   addCardToDex(card) {
+    //TODO: Check if card already in profile? increment
     if (this.state.dexProfile && card) {
       this.state.dexProfile.addCard(card);
     }
+  }
+
+  getErrorStatus() {
+    if (this.state.error) {
+      console.log(this.state.error);
+      setTimeout(() => this.setState({...this.state, error: null}), 10000);
+      return (
+        <Alert show={true} variant={this.state.error.type}>{this.state.error.component}</Alert>
+      );
+    }
+    return null;
   }
 
   render () {
     // <Route render={() => (
     //   <NotFound dexProfile={this.state.dexProfile}/>
     // )}/>
+    const errorFunc = (error) => {
+      this.setState({...this.state, error: error})
+    };
     return (
       <Router>
         <Switch>
-          {!this.state.dexProfile &&
-            <LoadDex onLoadedDex={(dexProfile) => this.setState({...this.state, dexProfile: dexProfile})}/>
-          }
-          {this.state.dexProfile && (
-            <div>
-              <Scanner addCardToDex={this.addCardToDex}/>
-              <NewCard addCardToDex={this.addCardToDex}/>
-              <Profile dexProfile={this.state.dexProfile}/>
-            </div>
-          )}
+          <div>
+            {this.getErrorStatus()}
+            {!this.state.dexProfile &&
+              <LoadDex onError={errorFunc} onLoadedDex={(dexProfile) => this.setState({...this.state, dexProfile: dexProfile})}/>
+            }
+            {this.state.dexProfile && (
+              <div>
+                <Scanner onError={errorFunc} addCardToDex={this.addCardToDex}/>
+                <NewCard onError={errorFunc} addCardToDex={this.addCardToDex}/>
+                <Profile onError={errorFunc} dexProfile={this.state.dexProfile}/>
+              </div>
+            )}
+          </div>
         </Switch>
       </Router>
     )
--- src/load.js
+++ src/load.js
@@ -1,3 +1,101 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './load.css';
+import { Route } from 'react-router-dom';
+import { Container, Row, Button, Toast, ProgressBar } from 'react-bootstrap';
+import { DexProfile } from './util.js';
+
+export class LoadDex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.main = React.createRef();
+    this.state = {
+      showLoad: false,
+      loadFile: null,
+      progress: -1,
+    }
+  }
+
+  async uploadFile(history) {
+    if (this.state.loadFile) {
+      const reader = new FileReader()
+      reader.addEventListener('load', (event) => {
+        const profile = DexProfile();
+        this.props.onLoadedDex(Object.assign(profile, JSON.parse(event.target.result)));
+        this.setState({...this.state, loadFile: null, showLoad: false, progress: -1});
+        history.push("/profile");
+      });
+      reader.addEventListener('progress', (event) => {
+        const progress = event.loaded / this.state.loadFile.size * 100;
+        this.setState({...this.state, progress: progress});
+      });
+
+      reader.readAsText(this.state.loadFile);
+    }
+  }
+
+  getLoadFile(history) {
+    const onCloseToast = () => {
+      if (this.state.showLoad) {
+        this.setState({...this.state, showLoad: false});
+      }
+    }
+    const verifyButton = () => {
+      if (this.state.loadFile && this.state.progress === -1) {
+        return (
+          <Button className="rounded-pill px-4 mt-2" onClick={() => this.uploadFile(history)}>Accept</Button>
+        );
+      }
+      return null;
+    }
+    const progressBar = () => {
+      if (this.state.progress >= 0) {
+        return (
+          <ProgressBar variant="success" className="mt-2" animated now={this.state.progress}/>
+        );
+      }
+      return null;
+    }
+    if (this.state.showLoad) {
+      return (
+        <Row className="justify-content-md-center mt-4">
+          <Toast onClose={onCloseToast}>
+            <Toast.Header>
+              <strong className="mr-auto">Load Dex</strong>
+            </Toast.Header>
+            <Toast.Body>
+              <input type="file" name=".dex file" accept=".dex" onChange={(event) => this.setState({...this.state, loadFile: event.target.files[0]})}/>
+              {verifyButton()}
+              {progressBar()}
+            </Toast.Body>
+          </Toast>
+        </Row>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <Route path="/" render={({history}) => (
+        <Container className="mt-4">
+          {this.getLoadFile(history)}
+          <Row className="justify-content-md-center">
+            <h1>PokIndex</h1>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Button className="rounded-pill px-4" onClick={() => this.setState({...this.state, showLoad: true})}>Load .dex file</Button>
+          </Row>
+          <Row className="justify-content-md-center">
+            <h2>or</h2>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Button className="rounded-pill px-4" onClick={() => {
+              this.props.onLoadedDex(new DexProfile());
+              history.push("/profile");
+            }}>Create new PokIndex</Button>
+          </Row>
+        </Container>
+      )}/>
+
+    );
+  }
+}
